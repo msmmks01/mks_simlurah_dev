fbd3ae5fcd40b266e2a269260ac4d38599738ebc
Yuniaervi29 <yuniaervinaeni@gmail.com>
2026-03-09 16:33:30 +0800

    rev nama model

--- __application/models/api.php
+++ __application/models/api.php
@@ -18,33 +18,31 @@ class Api extends CI_Model
 			$filter_status = "AND a.jab_rt_rw LIKE '%RW%'";
 		}
 
-		$sql = "
-		SELECT 
-		a.nik AS NIK,
-		a.nama_lengkap AS NAMA,
-		a.alamat AS ALAMAT,
-		a.no_npwp AS NPWP,
-		a.no_hp AS NO_HP,
-		a.no_rekening AS NOREK,
+		$sql = "SELECT 
+					a.nik AS NIK,
+					a.nama_lengkap AS NAMA,
+					a.alamat AS ALAMAT,
+					a.no_npwp AS NPWP,
+					a.no_hp AS NO_HP,
+					a.no_rekening AS NOREK,
 
-		CEIL(SUM(b.nilai)/COUNT(b.id)) AS JUMLAH,
+					CEIL(SUM(b.nilai)/COUNT(b.id)) AS JUMLAH,
 
-		a.jab_rt_rw AS STATUS,
-		b.bulan AS BULAN,
-		a.cl_kelurahan_desa_id AS KELURAHAN_ID
+					a.jab_rt_rw AS STATUS,
+					b.bulan AS BULAN,
+					a.cl_kelurahan_desa_id AS KELURAHAN_ID
 
-		FROM tbl_penilaian_rt_rw b
-		JOIN tbl_data_rt_rw a ON a.id = b.tbl_data_rt_rw_id
+					FROM tbl_penilaian_rt_rw b
+					JOIN tbl_data_rt_rw a ON a.id = b.tbl_data_rt_rw_id
 
-		WHERE
-		a.cl_kelurahan_desa_id = '$kelurahan_id'
-		AND a.status = 'Aktif'
-		AND a.pilih_tahun = '$tahun_login'
-		AND b.bulan = '$bulan'
-		$filter_status
+					WHERE
+					a.cl_kelurahan_desa_id = '$kelurahan_id'
+					AND a.status = 'Aktif'
+					AND a.pilih_tahun = '$tahun_login'
+					AND b.bulan = '$bulan'
+					$filter_status
 
-		GROUP BY a.id
-		";
+					GROUP BY a.id";
 
 		return $this->db->query($sql)->result();
 	}
