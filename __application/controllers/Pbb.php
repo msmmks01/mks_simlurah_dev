efae259786888caef33ebaf3aeb2b51185ab1fc1
Sabrang-hub <sabrangsham34@gmail.com>
2026-03-25 13:18:36 +0900

    Add connection timeout and verbose option to cURL settings in get_data_pbb method

--- __application/controllers/Pbb.php
+++ __application/controllers/Pbb.php
@@ -133,7 +133,10 @@ class Pbb extends JINGGA_Controller
 			],
 			CURLOPT_SSL_VERIFYPEER => false,
 			CURLOPT_SSL_VERIFYHOST => false,
-			CURLOPT_TIMEOUT        => 30
+			CURLOPT_TIMEOUT        => 30,
+			CURLOPT_CONNECTTIMEOUT => 10,
+			CURLOPT_VERBOSE => true
+
 		]);
 
 		$response = curl_exec($ch);
