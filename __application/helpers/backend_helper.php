89a967b591f8d0cc831219bb06c9709ec8172057
Sabrang-hub <sabrangsham34@gmail.com>
2026-03-26 13:25:16 +0900

    s

--- __application/helpers/backend_helper.php
+++ __application/helpers/backend_helper.php
@@ -813,4 +813,13 @@ if (!function_exists('randomize_letters')) {
 
         return $result;
     }
+
+    function get_csrf_token_name() {
+        $CI =& get_instance();
+        return $CI->security->get_csrf_token_name();
+    }
+    function get_csrf_hash() {
+        $CI =& get_instance();
+        return $CI->security->get_csrf_hash();
+    }
 }
