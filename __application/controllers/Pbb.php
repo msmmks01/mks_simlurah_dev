a7b11f26a58c422f6f2e2382f09a6858152925aa
Sabrang-hub <sabrangsham34@gmail.com>
2026-03-26 17:36:02 +0900

    ob_clean

--- __application/controllers/Pbb.php
+++ __application/controllers/Pbb.php
@@ -182,6 +182,7 @@ class Pbb extends JINGGA_Controller
 	private function _json($data)
 	{
 		header('Content-Type: application/json; charset=utf-8');
+		ob_clean();
 		echo json_encode($data);
 		exit;
 	}
