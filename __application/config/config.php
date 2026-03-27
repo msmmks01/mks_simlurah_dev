a3c2c86375223324a9178655c3c87386be1227c6
Yuniaervi29 <yuniaervinaeni@gmail.com>
2026-03-27 15:06:23 +0800

    penambahan dan revisi yunia

--- __application/config/config.php
+++ __application/config/config.php
@@ -101,6 +101,7 @@ $config['csrf_cookie_name'] = 'csrf_cookie';
 $config['csrf_expire'] = 7200;
 $config['csrf_regenerate'] = TRUE;
 $config['csrf_exclude_uris'] = array('backoffice-form/.*','backoffice-simpan/.*');
+// $config['csrf_exclude_uris'] = array();
 
 /* =========================================================
 | NETWORK
